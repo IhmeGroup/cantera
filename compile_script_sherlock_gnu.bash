890623c640cb7aa01df86221e1768275eb364ad8
Blake Andersen <blake.c.andersen@gmail.com>
2025-07-09 00:07:58 -0700

    Added compile_script for Sherlock GNU

---
+++ compile_script_sherlock_gnu.bash
@@ -0,0 +1,22 @@
+#! /bin/bash
+# Need to build SUNDIALS with -fPIC and set(CMAKE_MACOSX_RPATH 1)
+export CANTERA_DIR=$GB/local/cantera/2.4.0/
+export SUN_INCLUDE=/home/groups/mihme/banderse/local/sundials/build/include
+export SUN_LIB=/home/groups/mihme/banderse/local/sundials/build/lib
+export BOOST_INCLUDE=/home/groups/mihme/banderse/local/boost/boost_1_55_0/installationDir/include
+export EIGEN_INCLUDE=$GB/local/eigen/3.2.9/eigen/
+
+ml py-numpy/1.14.3_py27
+
+scons -j4 build prefix=$CANTERA_DIR \
+  CXX=g++ CC=gcc FORTRAN=gfortran python_package=none python3_package='n' \
+  optimize_flags='-O3' \
+  env_vars='all' \
+  sundials_include=$SUN_INCLUDE sundials_libdir=$SUN_LIB \
+  boost_inc_dir=$BOOST_INCLUDE f90_interface=y system_eigen=n \
+  extra_inc_dirs=$EIGEN_INCLUDE
+if [ -z "SCONS_TEST" ]; then
+    scons -j4 test
+fi
+scons -j4 install
+
